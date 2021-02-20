829488d9f7c0403c957eaa98111c6a8154eceb8a
Rukia <79343325+BeyondDevastation@users.noreply.github.com>
2021-02-20 13:34:59 +0000

    Add files via upload

---
+++ Dragon ball Simulator Farm.lua
@@ -0,0 +1,17 @@
+while true do
+game:GetService("ReplicatedStorage").Remotes.Combat:FireServer()
+
+game:GetService("ReplicatedStorage").Remotes.Combat2:FireServer()
+
+game:GetService("ReplicatedStorage").Remotes.Combat3:FireServer()
+
+game:GetService("ReplicatedStorage").Remotes.Combat4:FireServer()
+
+game:GetService("ReplicatedStorage").Remotes.Combat5:FireServer()
+
+game:GetService("ReplicatedStorage").Remotes.Combat6:FireServer()
+
+game:GetService("ReplicatedStorage").Remotes.Combat6:FireServer()
+
+wait()
+end
